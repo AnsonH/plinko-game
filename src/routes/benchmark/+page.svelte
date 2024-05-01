5bd6946e7562e8ddeee137ece891b97b2d8e45db
AnsonH <ansonheung2000@gmail.com>
2024-05-01 16:12:05 +0800

    refactor(benchmark): move to `/benchmark` route

---
+++ src/routes/benchmark/+page.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import BinsDistribution from '$lib/components/BinsDistribution.svelte';
+  import Plinko from '$lib/components/Plinko';
+  import { rowCountOptions } from '$lib/constants/game';
+  import { plinkoEngine, rowCount } from '$lib/stores/game';
+
+  let dropBallInterval: number | null = null;
+
+  let ballsDropped = 0;
+
+  function dropSingleBall() {
+    $plinkoEngine?.dropBall();
+    ballsDropped += 1;
+  }
+
+  function startDropBallInterval() {
+    dropBallInterval = setInterval(dropSingleBall, 10);
+  }
+
+  function stopDropBallInterval() {
+    if (dropBallInterval) {
+      clearInterval(dropBallInterval);
+      dropBallInterval = null;
+    }
+  }
+</script>
+
+<div>
+  <Plinko />
+</div>
+<div class="mx-4 my-8 flex items-center gap-16">
+  <div class="flex items-center gap-8">
+    <label for="rows">Rows</label>
+    <select bind:value={$rowCount} class="border border-gray-400 p-2">
+      {#each rowCountOptions as rows}
+        <option value={rows}>{rows}</option>
+      {/each}
+    </select>
+  </div>
+
+  <button on:click={dropSingleBall} class="bg-cyan-100 p-2">Drop Ball</button>
+
+  {#if dropBallInterval === null}
+    <button on:click={startDropBallInterval} class="bg-cyan-100 p-2">Start Auto Drop</button>
+  {:else}
+    <button on:click={stopDropBallInterval} class="bg-cyan-100 p-2">Stop Auto Drop</button>
+  {/if}
+
+  <p>Dropped: <span>{ballsDropped}</span></p>
+</div>
+
+<div class="mx-4 my-8">
+  <BinsDistribution />
+</div>
