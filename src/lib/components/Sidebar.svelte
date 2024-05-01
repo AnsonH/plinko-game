a78d497d4e32a33844251cc6b9d8d76d85cb786c
AnsonH <ansonheung2000@gmail.com>
2024-05-01 19:51:17 +0800

    feat: basic layout

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { rowCountOptions } from '$lib/constants/game';
+  import { plinkoEngine, rowCount } from '$lib/stores/game';
+</script>
+
+<div class="flex min-w-[300px] flex-col gap-4 bg-gray-700 p-3">
+  <div>
+    <label for="rowCount" class="text-sm font-medium text-gray-300">Rows</label>
+    <!-- TODO: Custom dropdown component -->
+    <select
+      id="rowCount"
+      bind:value={$rowCount}
+      class="block w-full rounded-sm border-2 border-gray-500 bg-gray-900 p-1 text-white"
+    >
+      {#each rowCountOptions as rows}
+        <option value={rows}>{rows}</option>
+      {/each}
+    </select>
+  </div>
+  <button
+    class="rounded-md bg-green-500 py-3 font-semibold text-gray-900 transition-colors hover:bg-green-400 active:bg-green-600"
+    on:click={() => $plinkoEngine?.dropBall()}
+  >
+    Drop ball
+  </button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,31 @@
 <script lang="ts">
   import Plinko from '$lib/components/Plinko';
+  import Sidebar from '$lib/components/Sidebar.svelte';
 </script>
 
-<div>
-  <Plinko />
+<div class="min-h-screen w-full">
+  <nav class="sticky top-0 w-full bg-gray-700 px-5 drop-shadow-lg">
+    <div class="mx-auto flex h-14 max-w-7xl items-center justify-between">
+      <!-- TODO: Design a logo -->
+      <p class="text-2xl font-medium text-white">Plinko</p>
+    </div>
+  </nav>
+  <div class="px-5">
+    <div class="mx-auto mt-5 max-w-7xl md:mt-10">
+      <div class="flex w-full overflow-hidden rounded-lg">
+        <Sidebar />
+        <div class="flex-1 bg-gray-900">
+          <div class="mx-auto h-full max-w-[760px]">
+            <Plinko />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
+
+<style>
+  :global(body) {
+    @apply bg-gray-800;
+  }
+</style>
