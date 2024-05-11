88686c8f72b80595f464d203893cab19a1a40eef
AnsonH <ansonheung2000@gmail.com>
2024-05-11 22:56:44 +0800

    feat(layout): credits footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,10 @@
   import Balance from '$lib/components/Balance.svelte';
   import Plinko from '$lib/components/Plinko';
   import Sidebar from '$lib/components/Sidebar.svelte';
+  import GitHubLogo from 'phosphor-svelte/lib/GithubLogo';
 </script>
 
-<div class="min-h-dvh w-full">
+<div class="flex min-h-dvh w-full flex-col">
   <nav class="sticky top-0 z-10 w-full bg-gray-700 px-5 drop-shadow-lg">
     <div class="mx-auto flex h-14 max-w-7xl items-center justify-between">
       <img src={logo} alt="logo" class="h-6 sm:h-7" />
@@ -14,7 +15,7 @@
       </div>
     </div>
   </nav>
-  <div class="px-5">
+  <div class="flex-1 px-5">
     <div class="mx-auto mt-5 min-w-[300px] max-w-xl md:mt-10 lg:max-w-7xl">
       <div class="flex flex-col-reverse overflow-hidden rounded-lg lg:w-full lg:flex-row">
         <Sidebar />
@@ -24,6 +25,33 @@
       </div>
     </div>
   </div>
+  <footer class="px-5 pb-4 pt-12">
+    <div class="mx-auto max-w-[40rem]">
+      <div aria-hidden="true" class="h-[1px] bg-slate-700" />
+      <div class="flex items-center justify-between p-2">
+        <p class="text-sm text-slate-500">
+          Created by
+          <a
+            href="https://www.ansonh.com"
+            target="_blank"
+            rel="noreferrer"
+            class=" text-cyan-600 transition hover:text-cyan-500"
+          >
+            Anson Heung
+          </a>
+        </p>
+        <a
+          href="https://github.com/AnsonH/plinko-game"
+          target="_blank"
+          rel="noreferrer"
+          class="flex items-center gap-1 p-1 text-sm text-slate-500 transition hover:text-cyan-500"
+        >
+          <GitHubLogo class="size-4" weight="bold" />
+          <span>Source Code</span>
+        </a>
+      </div>
+    </div>
+  </footer>
 </div>
 
 <style>
