e29d20307e0e3245043d1e693d0539f13e4e4f3b
AnsonH <ansonheung2000@gmail.com>
2024-11-19 13:46:12 +0800

    fix: handle `NaN` when parsing game balance from local storage

--- src/lib/utils/game.ts
+++ src/lib/utils/game.ts
@@ -3,13 +3,17 @@ import { balance } from '$lib/stores/game';
 import { get } from 'svelte/store';
 
 export function setBalanceFromLocalStorage() {
-  const balanceVal = window.localStorage.getItem(LOCAL_STORAGE_KEY.BALANCE);
-  if (balanceVal) {
-    balance.set(parseFloat(balanceVal));
+  const rawValue = window.localStorage.getItem(LOCAL_STORAGE_KEY.BALANCE);
+  const parsedValue = parseFloat(rawValue ?? '');
+  if (!isNaN(parsedValue)) {
+    balance.set(parsedValue);
   }
 }
 
 export function writeBalanceToLocalStorage() {
-  const balanceVal = get(balance).toFixed(2);
-  window.localStorage.setItem(LOCAL_STORAGE_KEY.BALANCE, balanceVal);
+  const balanceVal = get(balance);
+  if (!isNaN(balanceVal)) {
+    const balanceValStr = balanceVal.toFixed(2);
+    window.localStorage.setItem(LOCAL_STORAGE_KEY.BALANCE, balanceValStr);
+  }
 }
